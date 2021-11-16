7e8e602edf93ce568d28ba16be09ce03c5b41c88
AnastasiaMorozovaBPI205 <avmorozova_6@edu.hse.ru>
2021-11-16 20:51:26 +0300

    Initial Commit

---
+++ avmorozova_6PW6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avmorozova_6PW6
+//
+//  Created by Anastasia on 16.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
